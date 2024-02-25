2d9919a3f449c5f85dfafb8ba0cdbe02926c9129
Egil Bru <egilb4383@gmail.com>
2024-02-25 19:43:33 +0100

    Added my powershell C:\Users\egilb\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1

---
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+<# Powershell Profile #>
+<# File is ran on startup #>
+
+<# Theme #>
+oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/tokyonight_storm.omp.json | Invoke-Expression
+
+<# Windows Powershell Aliases  #>
+Function goto-nvimconfig {Set-Location -Path ~/AppData/Local/nvim}
+Set-Alias -Name nconfig -Value goto-nvimconfig
+
+Function saveto-git-repo {cp ~/Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 ~/AppData/Local/nvim/powershell}
+Set-Alias -Name sprofile saveto-git-repo
+clear
